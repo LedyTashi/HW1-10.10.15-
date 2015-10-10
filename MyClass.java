7647c0f155ecb3e8820d88148c9b6bc6370ac47c
LedyTashi <n_e_goncharova@mail.ru>
2015-10-10 19:19:03 +0300

    Update and rename README.md to MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,6 @@
+public class MyClass {
+    public static void main(String[] args) {
+        // этот текст появится на экране
+        System.out.println("Я рад учиться программировать!");
+    }
+}
